353f431552e5c8ca53027af825759f6248b31c67
tuituidear <penpong.futui.dear@gmail.com>
2024-11-05 01:50:27 +0700

    commit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import pandas as pd
 
-st.title("TESTTEST")
-df = pd.DataFrame(
-    {'first column' : [1,2,3,4],
-    'secound column' : [10,20,30,40]}
-)
-st.dataframe(df)
+# สร้างหน้า Streamlit
+st.title("NER Visualization with Spark NLP and Streamlit")
+text_input = st.text_area("Enter text for NER analysis", "Enter your text here...")
